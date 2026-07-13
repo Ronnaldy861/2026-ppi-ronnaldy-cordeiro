d77b9998ce2fa885220967aafe31ea1bb52d6f90
Ronnaldy861 <ronnaldy.s@escolar.ifrn.edu.br>
2026-07-13 13:52:42 +0000

    fix: implementar banco de dados SQLite (#3)

---
+++ src/equipamentos-ti/flaskr/schema.sql
@@ -0,0 +1,22 @@
+DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS equipamento;
+
+CREATE TABLE user (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL
+);
+
+CREATE TABLE equipamento (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome TEXT NOT NULL,
+    categoria TEXT NOT NULL,
+    fabricante TEXT NOT NULL,
+    patrimonio TEXT UNIQUE NOT NULL,
+    localizacao TEXT NOT NULL,
+    status TEXT NOT NULL,
+    usuario_id INTEGER NOT NULL,
+
+    FOREIGN KEY (usuario_id)
+        REFERENCES user(id)
+);
